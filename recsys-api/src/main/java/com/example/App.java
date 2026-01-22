bd5c90734157740ce89c05cf15b5144b63c55c6c
linghypshen@gmail.com <linghypshen@gmail.com>
2026-01-22 13:36:33 -0500

    initial setup

---
+++ recsys-api/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletContextHandler context = new ServletContextHandler();
+        context.setContextPath("/");
+        context.addServlet(HelloServlet.class, "/hello");
+
+        server.setHandler(context);
+
+        server.setStopAtShutdown(true);
+        server.start();
+        server.join();
+    }
+}
